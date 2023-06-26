110b7ed0c02bcbb8d448b9da052ca0bbe61e43d6
Kael <41486985+JyanseiKasch@users.noreply.github.com>
2023-06-26 00:31:25 -0700

    Update teste.pl

--- teste.pl
+++ teste.pl
@@ -2,3 +2,5 @@ livro(osmaias).
 nacionalidade(ecasequeiroz,portugues).
 autor(ecadequeiroz,osmaias).
 tipo(ecadequeiroz,romance).
+tipo(ecadequeiroz,ficcao).
+autorde2tipos(X):-tipo(X,romance),tipo(X,ficcao).
